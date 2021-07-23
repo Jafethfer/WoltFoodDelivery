184ad91f621f8508c31ac5a6e90ad0eb1598ff4c
Jafethfer <brando.fernandez@unah.hn>
2021-07-22 20:36:23 -0600

    Login y register components

--- frontend/Wolt-Food-Delivery/src/app/app-routing.module.ts
+++ frontend/Wolt-Food-Delivery/src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { SignupFormComponent } from './signup-form/signup-form.component';
 
-const routes: Routes = [];
+const routes:Routes = [
+  {
+    path: 'login', 
+    component: LoginFormComponent
+  },
+  {
+    path: 'signup',
+    component: SignupFormComponent
+  },
+  {
+    path: '',
+    component: LandingPageComponent
+  }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [LoginFormComponent, SignupFormComponent, LandingPageComponent]
