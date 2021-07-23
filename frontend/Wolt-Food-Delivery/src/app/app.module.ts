88abeef6e97379b55b62fa1a6acb277bd0f62015
Jafethfer <brando.fernandez@unah.hn>
2021-07-22 22:15:06 -0600

    Componentes y vistas

--- frontend/Wolt-Food-Delivery/src/app/app.module.ts
+++ frontend/Wolt-Food-Delivery/src/app/app.module.ts
@@ -4,12 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { UsuariosComponent } from './usuarios/usuarios.component';
+import { MotoristasComponent } from './motoristas/motoristas.component';
+import { AdministradoresComponent } from './administradores/administradores.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     routingComponents,
+    UsuariosComponent,
+    MotoristasComponent,
+    AdministradoresComponent,
   ],
   imports: [
     BrowserModule,
